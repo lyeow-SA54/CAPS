62463a0ee03b83d14f62eb6c39cfaa64cdfe4bd4
YI Ling <e0941863@u.nus.edu>
2022-06-23 18:33:11 +0800

    added image folder

--- team5/src/main/java/sg/edu/iss/team5/controllers/AdminCourseController.java
+++ team5/src/main/java/sg/edu/iss/team5/controllers/AdminCourseController.java
@@ -8,15 +8,20 @@ import java.util.Set;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,7 +50,9 @@ public class AdminCourseController {
 	@Autowired
 	private StudentService sService;
 	@Autowired LecturerService lService;
-//	
+	
+	
+
 //	@Autowired
 //	private UserValidator uValidator;
 //
